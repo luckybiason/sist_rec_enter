27f2df34ce9a01b8ae7b31a514e6ef87f7167d8f
fred <fred@fred-VirtualBox.(none)>
2013-09-26 23:03:01 -0300

    ajustes CSS

--- adm_rec/portal/static/js/vitrine/js/vitrine-product.js
+++ adm_rec/portal/static/js/vitrine/js/vitrine-product.js
@@ -1,8 +1,10 @@
 $(function(){
     $('#list').on('click', function(e){
+    	e.preventDefault();
         visualVitrineList();
     });
     $('#galery').on('click', function(e){
+    	e.preventDefault();
         visualVitrineGal();
     });
 });
@@ -10,8 +12,12 @@ $(function(){
 visualVitrineGal = function(e){
     var ref = $('#items-vitrine');
     ref.addClass('vitrine-galeria');
+    $("#galery").addClass('galery-actived');
+    $("#list").removeClass('list-actived');
 }
 visualVitrineList = function(e){
     var ref = $('#items-vitrine');
     ref.removeClass('vitrine-galeria');
+    $("#galery").removeClass('galery-actived');
+    $("#list").addClass('list-actived');
 }
